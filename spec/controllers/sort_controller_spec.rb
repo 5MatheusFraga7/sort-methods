06040992ce8434f531a314d320745def4351fdb1
5MatheusFraga7 <fragamatheus823@gmail.com>
2022-01-11 07:17:03 -0300

    ading-responders-gem

--- spec/controllers/sort_controller_spec.rb
+++ spec/controllers/sort_controller_spec.rb
@@ -7,4 +7,15 @@ RSpec.describe SortController, :type => :controller do
             expect(response.status).to eq(200)
         end
     end
+
+    describe "GET to_buble_sort -- Action of sort with buble sort method" do
+        it "has a 200 status code" do 
+            get :to_buble_sort
+            expect(response.status).to eq(200)
+        end
+
+        it "array was sorted" do 
+            ## 
+        end 
+    end
 end
